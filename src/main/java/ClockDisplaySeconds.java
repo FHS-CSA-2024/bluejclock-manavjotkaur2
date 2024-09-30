d7a2d916fadfa72b041b7130c4b33d11feab944d
manavjotkaur2 <manavbhullar22@gmail.com>
2024-09-30 11:05:33 -0700

    Update ClockDisplaySeconds.java
    Completed BlueJ Clock Program

--- src/main/java/ClockDisplaySeconds.java
+++ src/main/java/ClockDisplaySeconds.java
@@ -1,6 +1,42 @@
+package src.main.java;
+
 
 public class ClockDisplaySeconds
 {
-    //Extend ClockDisplaySeconds to include a seconds field.
-    //Update all methods and parameters to accomodate this change
+    private NumberDisplay hours;
+    private NumberDisplay minutes;
+    private NumberDisplay seconds;
+    
+    public ClockDisplaySeconds() {
+        this.hours = new NumberDisplay(24);
+        this.minutes = new NumberDisplay(60);
+        this.seconds = new NumberDisplay(60);
+    }
+    
+    public ClockDisplaySeconds(int hour, int minute, int second){
+        this.hours = new NumberDisplay(24);
+        this.minutes = new NumberDisplay(60);
+        this.seconds = new NumberDisplay(60);
+        setTime(hour,minute,second);
+    }
+    
+    public void setTime(int hour, int minute, int second){
+        this.hours.setValue(hour);
+        this.minutes.setValue(minute);
+        this.seconds.setValue(second);
+    }
+    
+    public String getTime() {
+        return hours.getDisplayValue() + ":" + minutes.getDisplayValue() + ":" + seconds.getDisplayValue();
+    }
+    
+    public void timeTick() {
+        seconds.increments();
+        if(seconds.getValue()==0){
+            minutes.increments();
+            if(minutes.getValue() == 0){
+                hours.increments();
+            }
+        }
+    }
 }
